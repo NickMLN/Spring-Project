1eaeb7022cd86ea932e5203b26c74f1c8f79eec0
nicolas.mlnascimento <moreiralima.nicolas@gmail.com>
2025-05-23 11:03:26 -0300

    Adicionado Interface Tipo Restaurante

---
+++ guiarestaurante/src/main/java/br/senac/sp/guiarestaurante/repository/TipoRestauranteRepository.java
@@ -0,0 +1,6 @@
+package br.senac.sp.guiarestaurante.repository;
+
+import br.senac.sp.guiarestaurante.model.TipoRestaurante;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TipoRestauranteRepository extends CrudRepository<TipoRestaurante, Long> { }
