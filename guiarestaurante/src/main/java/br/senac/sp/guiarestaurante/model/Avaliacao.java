03a418be49a239ecb5755d49a73d8a81c483bfbd
nicolas.mlnascimento <moreiralima.nicolas@gmail.com>
2025-05-30 10:25:46 -0300

    refetor Avaliacao.java

--- guiarestaurante/src/main/java/br/senac/sp/guiarestaurante/model/Avaliacao.java
+++ guiarestaurante/src/main/java/br/senac/sp/guiarestaurante/model/Avaliacao.java
@@ -1,5 +1,7 @@
 package br.senac.sp.guiarestaurante.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,8 +15,10 @@ public class Avaliacao {
     private long id;
     private double nota;
     private String comnetario;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private Calendar dataVisita;
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Restaurante restaurante;
     @ManyToOne
     private Usuario usuario;
